d5be4a2da26161a96c8e2f589222b48fc6470027
Hoc Can Huynh <canhuynh1998@gmail.com>
2022-01-08 23:38:15 -0800

    Merged Login feature to main branch

--- src/main/java/SelfBankingSystem/SelfBankingSystem/customer/CustomerController.java
+++ src/main/java/SelfBankingSystem/SelfBankingSystem/customer/CustomerController.java
@@ -1,8 +1,6 @@
 package SelfBankingSystem.SelfBankingSystem.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
